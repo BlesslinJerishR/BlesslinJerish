913988dec2e8b0eaf9c37928c901809dae79638d
Mastermindx33 <mastermindx33@gmail.com>
2021-08-01 22:38:37 +0530

    __x__hug

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Icon } from '@components/icons';
 import { socialMedia } from '@config';
-import hug from './images/hug.png'
+import hug from '../images/hug.png'
 
 const StyledFooter = styled.footer`
   ${({ theme }) => theme.mixins.flexCenter};
