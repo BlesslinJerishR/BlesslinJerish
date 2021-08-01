eab9d437a74a84bca2033f6a22f7d0ce99c39ea0
Mastermindx33 <mastermindx33@gmail.com>
2021-08-01 22:48:04 +0530

    __r__hug

--- src/components/footer.js
+++ src/components/footer.js
@@ -107,7 +107,7 @@ const Footer = () => {
 
       <StyledCredit tabindex="-1">
         <a href="https://github.com/Mastermindx33/BlesslinJerishR">
-          <div>Made with <img src={hug} alt="hug"/> &amp; JavaScript
+          <div>Made with <img src={hug} alt="hug" height="30px" width="30px"/> &amp; JavaScript
                by Blesslin Jerish
           </div>
         </a>
