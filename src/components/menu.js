2154f60797a89762c9bdd9ae4a1afddec6276182
Mastermindx33 <mastermindx33@gmail.com>
2021-08-13 16:11:39 +0530

    __build__

--- src/components/menu.js
+++ src/components/menu.js
@@ -266,7 +266,7 @@ const Menu = () => {
               </ol>
             )}
 
-            <a href="/Blesslin_Resume.pdf" className="resume-link">
+            <a href="/resume.pdf" className="resume-link">
               Resume
             </a>
           </nav>
